d07a20720af65006403502e782e6bbcc7ec5b788
Faqih Indra Lesmana <107176002+faqihindrll@users.noreply.github.com>
2023-12-18 23:27:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,5 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
     
